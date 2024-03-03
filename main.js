3024c6b9b48a3bff31688a826a530280fb6a0060
azizbek400 <aziznormurodov400@gmail.com>
2024-03-03 23:59:52 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,272 @@
+//  BOLEAN 1    ////////////////////////////
+
+// let a = 40;
+// console.log(a > 0);
+
+
+//  BOLEAN 2     ////////////////////////////
+
+// const a = 15;
+
+// if(a % 2 == 1) {
+//     console.log("toq son");
+// }else {
+//     console.log("juft son");
+// }
+
+
+//  BOLEAN 3    ////////////////////////////
+
+// const a = 30;
+
+// if(a % 2 == 1) {
+//     console.log("toq son");
+// }else {
+//     console.log("juft son");
+// }
+
+//  BOLEAN 4     ////////////////////////////
+
+// let a = 4;
+// let b = 3;
+// console.log(a > 2 );
+// console.log(b <= 3);
+
+
+//  BOLEAN 5     ////////////////////////////
+
+// let a  = 0;
+// let b = -5;
+
+// console.log(a >= 0);
+// console.log(b < -2);
+
+//  BOLEAN 6     ////////////////////////////
+
+// let a = 5;
+// let b = 5.3;
+// let c = 5.9;
+
+// console.log(a <= b <= c );
+
+//  BOLEAN 7     ////////////////////////////
+
+// let a = 15;
+// let b = 20;
+// let c = 47;
+
+// console.log(a <= b <= c);
+
+//  BOLEAN 8     ////////////////////////////
+
+// let a = 15;
+// let b = 31;
+
+// if (a % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (b % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+//  BOLEAN 9    ////////////////////////////
+
+// let a = 16;
+// let b = 9;
+
+// if (a % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (b % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+
+//  BOLEAN 10    ////////////////////////////
+
+// let a = 90;
+// let b = 31;
+
+// if (a % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (b % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+
+//  BOLEAN 11    ////////////////////////////
+
+// let a = 16;
+// let b = 32;
+
+// if (a % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (b % 2 === 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+//  BOLEAN 12    ////////////////////////////
+
+// let a = 10;
+// let b = 25;
+// let c = 42;
+ 
+// if (a > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (b > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (c > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+//  BOLEAN 13    ////////////////////////////
+
+// let a = 10;
+// let b = -42;
+// let c = -76;
+ 
+// if (a > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (b > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// if (c > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+//  BOLEAN 16    ////////////////////////////
+
+// let a = 58;
+// console.log(10 < a < 99);
+// console.log(a % 2=== 0);
+
+
+// BOLEAN 17   ////////////////////////////
+
+// let a = 155;
+
+// console.log(100 < a < 999);
+// console.log(a % 2 === 1);
+
+// BOLEAN 18   ////////////////////////////
+
+// let a = 10;
+// let b = 10.5;
+// let c = 76;
+
+// console.log(a <= b);
+// console.log(a > b);
+
+
+
+// for 1   ////////////////////////////
+
+// let n = 15;
+
+// for (let k = 0; k < n; k++) {
+//     console.log(n);
+    
+// }
+
+
+// for 2   ////////////////////////////
+
+// let a = 20;
+
+// for (let b = 0; b <= a; b++) {
+//     console.log(b);
+// }
+
+
+// for 4   ////////////////////////////
+
+// let a = 4;
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i, i * a);
+// }
+
+
+//  BOLEAN 17    ////////////////////////////
+
+// const a = 323;
+
+// if(a % 2 == 1) {
+//     console.log("True");
+// }else {
+//     console.log("false");
+// }
+
+
+//  BOLEAN 23    ////////////////////////////
+
+// let a = 323;
+// console.log(a);
+
+
+//  BOLEAN 16    ////////////////////////////
+
+// const a = 90;
+
+// if(a % 2 == 1) {
+//     console.log("toq son");
+// }else {
+//     console.log("juft son");
+// }
+
+
+// for 5  ////////////////////////////
+
+// let a = 50;
+
+// for (let i = 0.1; i < 1; i=i+0.1) {
+//    console.log(i, i * a);
+// }
+
+
+
+
+
+
+
